2b512b8dcb556249ea18cb1a0f22dba4df37c518
Chiru9160 <chiranjeevi4.adarsh@gmail.com>
2025-06-21 18:42:39 +0530

    Add files via upload

--- backned/pdfProcessor.js
+++ backned/pdfProcessor.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import pdf2table from "pdf2table";
 import { getStudentModel } from "./Models/student.js";
 import BranchPerformance from './Models/branchPerformance.js';
-import { sendResultEmail } from './config/EmailConfig.js';
+import { sendResultEmail } from './Config/EmailConfig.js';
 
 export async function processPdf(filePath, semester, processId) {
     if (!semester || typeof semester !== "string") {
